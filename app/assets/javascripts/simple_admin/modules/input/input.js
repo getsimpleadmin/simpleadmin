ca10bed3a6453faed5578981be6d4f6b616b67bd
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-07-21 16:42:38 +0300

    fix font issue and aside navigation bug

---
+++ app/assets/javascripts/simple_admin/modules/input/input.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+(function (root, factory) {
+	// AMD
+	if (typeof define === 'function' && define.amd) {
+		define(function () {
+			return factory(root);
+		});
+	}
+	// Node.js or CommonJS
+	else if (typeof exports === 'object') {
+		module.exports = factory;
+	}
+
+	// Browser globals
+	else {
+		root.input = factory(root);
+	}
+})(this, function (root) {
+	'use strict';
+	var instance = {
+		nodeSelector: '.input',
+		classes: {error: 'form__field--error'}
+	};
+	instance.onChange = function() {
+		if ($(this).hasClass(instance.classes.error)) {
+			$(this).removeClass(instance.classes.error);
+		}
+	};
+	instance.init = function() {
+		$(instance.nodeSelector).on('keyup', instance.onChange);
+	};
+
+	return instance;
+});
+
+$(document).ready(function() {
+	input.init();
+});
