c43f173d3c1576b5ca067fb8b1d154db051f4287
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-07-27 16:01:11 +0300

    Improved test examples

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    admin_posts_path(current_locale)
+  end
+
+  def current_locale
+    @current_locale ||= { locale: SimpleAdmin::Setting.default_language }
+  end
+
+  helper_method :current_locale
+end
