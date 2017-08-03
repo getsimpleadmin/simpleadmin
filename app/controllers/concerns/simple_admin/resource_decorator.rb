43f4076b92705ff5496045874f42906080505801
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-08-03 13:15:11 +0300

    Added decorators

---
+++ app/controllers/concerns/simple_admin/resource_decorator.rb
@@ -0,0 +1,17 @@
+module SimpleAdmin
+  module ResourceDecorator
+    extend ActiveSupport::Concern
+
+    included do
+      def self.controller_name
+        name.gsub(/(^(.+?)::Admin::)/, '')
+      end
+
+      def self.decorator_class
+        "#{controller_name}Decorator".constantize rescue nil
+      end
+
+      include decorator_class if self.decorator_class.present?
+    end
+  end
+end
