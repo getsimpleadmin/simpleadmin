4b1e19145a9ebff24dde33b5fecf67df4ba8127c
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-08-07 19:44:28 +0300

    Added demo data migration

--- lib/generators/templates/migrations/migration.rb
+++ lib/generators/templates/migrations/migration.rb
@@ -1,25 +1,21 @@
 module Migrations
   def change
-    def change
-      create_table :simple_admin_users do |t|
-        t.string :email,              null: false, default: ''
-        t.string :encrypted_password, null: false, default: ''
+    create_table :simple_admin_users do |t|
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
-        t.string   :reset_password_token
-        t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-        t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
-        t.integer  :sign_in_count, default: 0, null: false
-        t.datetime :current_sign_in_at
-        t.datetime :last_sign_in_at
-        t.inet     :current_sign_in_ip
-        t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
-        t.timestamps null: false
-      end
-
-      add_index :users, :reset_password_token, unique: true
+      t.timestamps null: false
     end
 
     create_table :simple_admin_pages do |t|
@@ -104,6 +100,8 @@ module Migrations
 
       t.timestamps null: false
     end
+
+    add_index :simple_admin_users, :reset_password_token, unique: true
   end
 end
 
