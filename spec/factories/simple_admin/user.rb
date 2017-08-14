0cbb55fa2d6af7a417af3ec785e23bd6c0028eb7
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-08-14 18:48:23 +0300

    Added truncate for text field

--- spec/factories/simple_admin/user.rb
+++ spec/factories/simple_admin/user.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user, class: SimpleAdmin::User do
     email { 'demo@example.com' }
+    password { 'example' }
 
     after(:create) do |user|
       create(:profile, user: user)
