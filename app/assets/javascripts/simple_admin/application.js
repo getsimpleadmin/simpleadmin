c0f4c7ad825ba3d167beffae552d3a4b5cbb77c4
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-08-30 19:00:20 +0300

    Mockup reintegration

--- app/assets/javascripts/simple_admin/application.js
+++ app/assets/javascripts/simple_admin/application.js
@@ -1,20 +1,14 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require jquery
-//= require best_in_place
-//= require simple_admin/tabs
-//= require simple_admin/notify
-//= require simple_admin/tags
-//= require simple_admin/change-password
+//= require simple_admin/vendor/jquery_simple_admin
+//= require simple_admin/vendor/jquery.tablesorter.min
+//= require simple_admin/modules/nav-group/nav-group
+//= require simple_admin/modules/form/form
+//= require simple_admin/modules/image/image
+//= require simple_admin/modules/input/input
+//= require simple_admin/modules/table/table
+//= require simple_admin/modules/textarea/textarea
+//= require simple_admin/modules/aside/aside
+//= require simple_admin/modules/notify/notify
 //= require simple_admin/core
+//= require best_in_place
