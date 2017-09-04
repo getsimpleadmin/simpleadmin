56d7df143b9e92a6be15ed685e706d07cd658721
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-09-04 15:40:49 +0300

    Removed tabs

--- app/assets/javascripts/simple_admin/core.js
+++ app/assets/javascripts/simple_admin/core.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  if(!!$('#tabs').length) {
-    $('#tabs').tabs();
-  }
-
   if(!!$('#tags').length) {
     $('#tags').tagsInput({ width: '100%', height: '36px' });
   }
