15e01e5867f680a5dc2594b6f103baa27017990f
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-09-05 17:03:19 +0300

    Add taggable

--- lib/simple_admin/engine.rb
+++ lib/simple_admin/engine.rb
@@ -1,12 +1,15 @@
 require 'jquery-rails'
 require 'sass-rails'
 
+require 'best_in_place'
+
 require 'devise'
 require 'kaminari'
+require 'acts-as-taggable-on'
+
 require 'rolify'
 require 'cancancan'
 require 'aasm'
-require 'best_in_place'
 
 module SimpleAdmin
   class Engine < Rails::Engine
