b078eceb45c3f1a69a4fe2122d419b929363057b
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-09-08 14:30:30 +0300

    Improved featured plugin

--- app/controllers/simple_admin/admin/plugins/featured/feature_items_controller.rb
+++ app/controllers/simple_admin/admin/plugins/featured/feature_items_controller.rb
@@ -4,9 +4,12 @@ module SimpleAdmin
       module Featured
         class FeatureItemsController < BaseController
           def autocomplete
-            @posts = SimpleAdmin::Post.with_translations(:en).where('title LIKE ?', "%#{params[:title]}%")
+            posts =
+              SimpleAdmin::Post.with_translations(:en).where('title LIKE ?', "%#{params[:title]}%").map do |post|
+                { id: post.id, title: post.title }
+              end
 
-            render json: @posts
+            render json: posts
           end
         end
       end
