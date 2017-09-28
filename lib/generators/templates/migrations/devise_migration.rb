7b70aa3b57456508c8904e2d5c85fa94619aae7c
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-09-28 22:51:27 +0300

    Fix devise sqlite3 migration

--- lib/generators/templates/migrations/devise_migration.rb
+++ lib/generators/templates/migrations/devise_migration.rb
@@ -12,8 +12,14 @@ class CreateSimpleAdminDeviseMigration < ActiveRecord::Migration[5.1]
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      
+      if ActiveRecord::Base.connection.adapter_name == 'postgresql'
+        t.inet :current_sign_in_ip
+        t.inet :last_sign_in_ip
+      else
+        t.string :current_sign_in_ip
+        t.string :last_sign_in_ip
+      end
 
       t.timestamps null: false
     end
