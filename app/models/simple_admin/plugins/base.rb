7a4d108bfbce19b152e706814a6ca04cc735db7a
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-09-29 13:18:11 +0300

    Removed plugins

--- app/models/simple_admin/plugins/base.rb
+++
@@ -1,8 +0,0 @@
-module SimpleAdmin
-  module Plugins
-    class Base < ApplicationRecord
-      self.abstract_class = true
-      self.table_name_prefix = 'simple_admin_plugins_'
-    end
-  end
-end
