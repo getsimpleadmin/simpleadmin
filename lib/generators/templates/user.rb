46168445760b1dacb7d8e33e0ee47dfc6e229e77
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-10-05 16:33:43 +0300

    Add devise views generation

---
+++ lib/generators/templates/user.rb
@@ -0,0 +1,35 @@
+module SimpleAdmin
+  class User < Base
+    rolify role_cname: 'SimpleAdmin::Role'
+
+    has_one :profile, dependent: :destroy
+
+    has_many :posts
+    has_many :comments
+
+    accepts_nested_attributes_for :profile, update_only: true
+    delegate :avatar, :first_name, :last_name, to: :profile, allow_nil: true
+
+    after_create :create_profile!
+
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+
+    # TODO: HARDCODE
+    def role
+      roles.first
+    end
+
+    def permissions
+      SimpleAdmin::UserPermission.joins(:role).where(simple_admin_roles: { name: role.name })
+    end
+
+    private
+
+      def create_profile!
+        profile = build_profile
+        profile.save
+      end
+  end
+end
