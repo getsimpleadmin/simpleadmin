3ded3d36fa95cf638eb79b898f1c2772fdbc800c
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-10-11 22:00:12 +0300

    Removed unnecessary user columns

--- lib/generators/templates/migrations/core_migrations/users_migration.rb
+++ lib/generators/templates/migrations/core_migrations/users_migration.rb
@@ -4,23 +4,6 @@ class CreateSimpleAdminUsersMigration < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      t.datetime :remember_created_at
-
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-
-      if ActiveRecord::Base.connection.adapter_name == 'postgresql'
-        t.inet :current_sign_in_ip
-        t.inet :last_sign_in_ip
-      else
-        t.string :current_sign_in_ip
-        t.string :last_sign_in_ip
-      end
-
       t.timestamps null: false
     end
   end
