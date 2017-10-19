cbf3ef2c709ffef1bf1c6070253c0ff90b1d69ca
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-10-19 07:42:43 +0300

    Add code improvements and documentation for MenuDsl

--- lib/simple_admin/menu_dsl.rb
+++ lib/simple_admin/menu_dsl.rb
@@ -2,6 +2,22 @@ module SimpleAdmin
   module MenuDsl
     extend self
 
+    # Menu DSL methods
+    #
+    # @example
+    #  SimpleAdmin::Config.setup do |config|
+    #    config.menu.configure do
+    #      item do
+    #        label { 'Settings' }
+    #        icon  { 'si-settings' }
+    #
+    #        route { resources :settings, only: [:index, :update] }
+    #      end
+    #    end
+    #  end
+    #
+    # @since 0.1.1
+
     def configure(&block)
       instance_eval &block
     end
