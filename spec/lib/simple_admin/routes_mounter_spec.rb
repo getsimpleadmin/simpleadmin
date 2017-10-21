0bd123ee1194bbfc3040e03280d6dd0b7014b569
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-10-21 17:55:02 +0300

    Add test example

--- spec/lib/simple_admin/routes_mounter_spec.rb
+++ spec/lib/simple_admin/routes_mounter_spec.rb
@@ -9,20 +9,23 @@ RSpec.describe SimpleAdmin::RoutesMounter do
     let(:menu_system_routes) { [menu_route] }
 
     before do
-      expect(described_class).to receive(:menu_system_routes).and_return(menu_system_routes)
-      expect(routing_mapper).to receive(:public_send).with(:resources, :settings, { only: %i[index update] })
+      allow(described_class).to receive(:menu_system_routes).and_return(menu_system_routes)
     end
 
     subject { described_class.mount_system_routes!(routing_mapper) }
 
-    # it 'returs mounter routes' do
-    #   subject
-    #
-    #   expect(m)
-    # end
-    it { subject }
+    it 'return mounted routes' do
+      expect(routing_mapper).to receive(:public_send).with(:resources, :settings, { only: %i[index update] })
+      subject
+    end
 
     context 'when params empty' do
+      let(:menu_route) { build :menu_route, params: nil }
+
+      it 'replace params with empty hash' do
+        expect(routing_mapper).to receive(:public_send).with(:resources, :settings, {})
+        subject
+      end
     end
   end
 end
