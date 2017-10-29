6e7f8642569a7b564a1c8fa9cb80955061ec44e5
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-10-29 20:34:36 +0300

    Removed unused method

--- app/helpers/simple_admin_helper.rb
+++ app/helpers/simple_admin_helper.rb
@@ -1,10 +1,6 @@
 module SimpleAdminHelper
   include SimpleAdmin::Helpers::UrlHelper
 
-  def render_plugin_actions(plugin)
-    render "simple_admin/plugins/#{plugin.name}/actions", plugin: plugin
-  end
-
   def resources_pagination_start(per_page, current_page)
     (per_page * current_page.to_i) - (per_page - 1)
   end
